69ccdf9fe019c06b2c0354f9dffbc7ffa23f4987
vini_nascimento@outlook.com <vinicius nascimento>
2025-11-03 01:04:48 -0300

    alteração nas rotas pois estava dando conflito

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthWebController;
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CategoryWebController;
-use App\Http\Controllers\OrderController;
 use App\Http\Controllers\OrderWebController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductWebController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,35 +12,6 @@ Route::get('/', function () {
 
 Route::get('/login', [AuthWebController::class, 'showLogin'])->name('login');
 Route::get('/register', [AuthWebController::class, 'showRegister'])->name('register');
-
-Route::prefix('api')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-
-    Route::get('/products', [ProductController::class, 'index']);
-    Route::get('/products/{id}', [ProductController::class, 'show']);
-
-    Route::get('/categories', [CategoryController::class, 'index']);
-    Route::get('/categories/{id}', [CategoryController::class, 'show']);
-
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-        
-        Route::apiResource('orders', OrderController::class);
-        
-        Route::middleware('can:manage-products')->group(function () {
-            Route::post('/products', [ProductController::class, 'store']);
-            Route::put('/products/{id}', [ProductController::class, 'update']);
-            Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-        });
-        
-        Route::middleware('can:manage-categories')->group(function () {
-            Route::post('/categories', [CategoryController::class, 'store']);
-            Route::put('/categories/{id}', [CategoryController::class, 'update']);
-            Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
-        });
-    });
-});
 
 Route::get('/products', [ProductWebController::class, 'index'])->name('products.index');
 Route::get('/products/create', [ProductWebController::class, 'create'])->name('products.create');
